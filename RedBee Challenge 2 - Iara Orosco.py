04e1a0137e8ef5bdbe5b574e981dd5e7eb75ac2b
Iara Denise Orosco <76779189+oroscoiara@users.noreply.github.com>
2021-06-18 13:02:23 -0300

    Add files via upload

---
+++ RedBee Challenge 2 - Iara Orosco.py
@@ -0,0 +1,10 @@
+#SumArrayNumbers: That receives a list and return the sum of entire list
+
+arr1= [10, 12, 20]
+arr2= [6, 10, 7]
+arr3 = sum(arr1) + sum(arr2)
+
+print("La suma de los elementos del primer array es de:" ,sum(arr1))
+print("La suma de los elementos del segundo array es de:" ,sum(arr2))
+print("La suma de ambos arrays es de:" ,arr3)
+
